00c68c8f307e081f37b7c3f96b763829648d0897
Noor <=>
2018-10-11 09:50:31 -0400

    Reset the company model to the previous version.

--- app/src/main/java/com/fzhongfei/findzhongfei_final/model/Company.java
+++ app/src/main/java/com/fzhongfei/findzhongfei_final/model/Company.java
@@ -1,36 +1,26 @@
 package com.fzhongfei.findzhongfei_final.model;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Company {
 
     private int id;
-    private String logoImage;
+    private int image;
     private String compName, shortDesc, compType;
     private double rating;
 
-    public Company(int id, String logoImage, String compName, String compType, String shortDesc, double rating) {
+    public Company(int id, int logoImage, String compName, String compType, String shortDesc, double rating) {
         this.id = id;
-        this.logoImage = logoImage;
+        this.image = logoImage;
         this.compName = compName;
         this.shortDesc = shortDesc;
         this.rating = rating;
         this.compType = compType;
     }
-    public void SetCompData(HashMap<String, String> compData)
-    {
-        this.id = Integer.parseInt(compData.get("id"));
-        this.logoImage = compData.get("logoImage");
-        this.compName = compData.get("compName");
-        this.compType = compData.get("compType");
 
-    }
     public int getId() {
         return id;
     }
-    public String getImage() {
-        return this.logoImage;
+    public int getImage() {
+        return image;
     }
     public String getCompName() {
         return compName;
